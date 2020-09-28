898cd35a6fb60e72bc8f6aa66669be68c168ecd6
Rxin1019 <15958741067@163.com>
2020-09-28 22:17:49 +0800

    首页全部完成

--- php/taobao.php
+++ php/taobao.php
@@ -1,7 +1,23 @@
 <?php
+header('content-type:text/html;charset=utf-8');
+define('HOST','localhost');
+define('USERNAME','root');
+define('PASSWORD','root');
+define('DBNAME','jzx');
+$conn = @new mysqli(HOST,USERNAME,PASSWORD,DBNAME);
+if($conn->connect_error){
+    die('数据库连接失败'.$conn->connect_error);
+}
+//详情页
+if(isset($_GET['sid'])){
+    $sid = $_GET['sid'];
+    $result = $conn->query("select * from taobao where sid=$sid");
+    echo json_encode($result->fetch_assoc());
+}
+//数据渲染
+$conn->query('SET NAMES UTF8');
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Method:POST,GET');
-include "conn.php";
 $result = $conn->query("select * from taobao");
 $arr = array();
 for($i=0;$i<$result->num_rows;$i++){
--- src/script/index_module.js
+++ src/script/index_module.js
@@ -4,7 +4,7 @@ define([], function() {
             //热卖单品渲染
             const $list = $('.sale-bd');
             $.ajax({
-                    url: 'http://localhost/p/xiangmu/php/taobao.php',
+                    url: 'http://192.168.13.75/p/xiangmu/php/taobao.php',
                     dataType: 'json'
                 })
                 .done((data) => {
@@ -41,7 +41,7 @@ define([], function() {
             //猜你喜欢渲染
             const $list2 = $('.six-floor .list');
             $.ajax({
-                    url: 'http://localhost/p/xiangmu/php/taobao.php',
+                    url: 'http://192.168.13.75/p/xiangmu/php/taobao.php',
                     dataType: 'json'
                 })
                 .done((data) => {
@@ -149,7 +149,7 @@ define([], function() {
             $('.inner-lf li').on('mouseover', function() {
                 // $(this).addClass('active').siblings().removeClass('active');
                 $('.inner-lf .cartlist .item-t').eq($(this).index()).show().siblings('.item-t').hide();
-                $('.inner-lf .cartlist').show();
+                $('.inner-lf .cartlist').fadeIn("fast");
             });
             $('.inner-lf li').on('mouseout', function() {
                 $('.inner-lf .cartlist').hide();
@@ -160,8 +160,157 @@ define([], function() {
                 $('.inner-lf .cartlist').hide();
             });
             //第一个轮播图
+            let $liW = $('.lunbo li').first().width();
+            let index = null;
+            $('.lunbo').width($('.lunbo li').size() * $liW + 'px');
+            $(".circleNav li").click(function() { //小圆点切换图片
+                index = $(this).index() - 1;
+                move();
+            });
+            let $timer = setInterval(move, 3000); //自动轮播
+            function move() { //右移
+                index++;
+                if (index === $(".circleNav li").length + 1) {
+                    $('.lunbo').css({
+                        left: 0
+                    })
+                    index = 1
+                }
+                if (index === $(".circleNav li").length) {
+                    $(".circleNav li").eq(0).addClass("select").siblings().removeClass("select");
+                }
+                if (index === -1) {
+                    $('.lunbo').css({
+                        left: -($('.lunbo li').length - 1) * $liW + 'px'
+                    })
+                    index = $(".circleNav li").length - 1;
+                }
+                $(".circleNav li").eq(index).addClass("select").siblings().removeClass("select");
+                $(".lunbo").stop(true).animate({ left: -$liW * index });
+            };
+            $('.arrow-l').click(function() { //点击左箭头
+                index -= 2;
+                move();
+            });
+            $('.arrow-r').click(function() { //点击右箭头
+                move();
+            });
+            $(".pic-box").hover(function() { //鼠标移入暂停，移出继续
+                    $('.arrow-l').css({
+                        display: 'block'
+                    });
+                    $('.arrow-r').css({
+                        display: 'block'
+                    })
+                    clearInterval($timer);
+                },
+                function() {
+                    $('.arrow-l').css({
+                        display: 'none'
+                    });
+                    $('.arrow-r').css({
+                        display: 'none'
+                    })
+                    $timer = setInterval(move, 3000);
+                });
 
-            //右侧tab切换
+            //第二个轮播图
+            let $liW2 = $('.lunbo2 li').first().width();
+            let index2 = null;
+            $('.lunbo2').width($('.lunbo2 li').size() * $liW2 + 'px');
+            let $timer2 = setInterval(move2, 3000); //自动轮播
+            function move2() { //右移
+                index2++;
+                if (index2 === $(".solidNav li").length + 1) {
+                    $('.lunbo2').css({
+                        left: 0
+                    })
+                    index2 = 1;
+                }
+                if (index2 === $(".solidNav li").length) {
+                    $(".solidNav li").eq(0).addClass("selected").siblings().removeClass("selected");
+                    $('.head-inner strong i').text(1);
+                } else {
+                    $('.head-inner strong i').text(index2 + 1);
+                }
+                if (index2 === -1) {
+                    $('.lunbo2').css({
+                        left: -($('.lunbo2 li').length - 1) * $liW + 'px'
+                    })
+                    index2 = $(".solidNav li").length - 1;
+                }
+                $(".solidNav li").eq(index2).addClass("selected").siblings().removeClass("selected");
+                $(".lunbo2").stop(true).animate({ left: -$liW2 * index2 });
+
+            };
+            $('.arrow2-l').click(function() { //点击左箭头
+                index2 -= 2;
+                move2();
+            });
+            $('.arrow2-r').click(function() { //点击右箭头
+                // if(in)
+                move2();
+            });
+            $(".lb2").hover(function() { //鼠标移入暂停，移出继续
+                    $('.arrow2-l').css({
+                        display: 'block'
+                    });
+                    $('.arrow2-r').css({
+                        display: 'block'
+                    })
+                    clearInterval($timer2);
+                },
+                function() {
+                    $('.arrow2-l').css({
+                        display: 'none'
+                    });
+                    $('.arrow2-r').css({
+                        display: 'none'
+                    })
+                    $timer2 = setInterval(move2, 3000);
+                });
+
+            //楼梯效果
+            $(window).on('scroll', function() {
+                if ($(window).scrollTop() > 500) {
+                    $('.hide-search').slideDown();
+                    $('.fixedtool').css({
+                        position: 'fixed',
+                        top: '75px'
+                    })
+                } else {
+                    $('.hide-search').hide();
+                    $('.fixedtool').css({
+                        position: 'absolute',
+                        top: '580px',
+                        right: '290px'
+                    });
+                }
+
+                $('.floor').each(function(index) {
+                    let $height = $(this).offset().top;
+                    if ($height > $(window).scrollTop()) {
+                        $('.fixedtool a').eq(index).addClass('on').siblings('a').removeClass('on');
+                        return false;
+                    }
+                })
+            })
+            $('.fixedtool a').click(function() {
+                $(this).addClass('on').siblings('a').removeClass('on');
+            })
+
+            $('.bt').click(function() {
+                let $loutiTop = $('.floor').eq($(this).index() - 1).offset().top - 40;
+                $('html').animate({
+                    scrollTop: $loutiTop
+                })
+            })
+            $('.fixed-5').click(function() {
+                    $('html').animate({
+                        scrollTop: 0
+                    })
+                })
+                //右侧tab切换
             $('.notice .title li').hover(function() {
                 $('.notice .title li a').removeClass('active');
                 $(this).children().addClass('active');
